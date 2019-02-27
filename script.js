0776465002340449d4471472dd40d872eecde804
Baheya Khalifa <beya@Baheyas-MacBook-Pro.local>
2019-02-27 23:58:10 +0200

    added code to create our bootstrap grid using jquery instead of HTML, but still need to add the code that adds all the products to the grid.

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
 var book1 = {
     "name": "The Little Prince",
-    "category": "book",
+    "category": "Book",
     "price": 100,
     "picture_url": "https://images-na.ssl-images-amazon.com/images/I/710H4c%2BmDeL.jpg"
 };
 var book2 = {
     "name": "The City Always Wins",
-    "category": "book",
+    "category": "Book",
     "price": 250,
     "picture_url": "https://www.easons.com/globalassets/5637150827/all/books/fiction/fiction-a-to-z/contemporary-fiction/9780571335176.jpg"
 };
 var book3 = {
     "name": "The Map of Love",
-    "category": "book",
+    "category": "Book",
     "price": 100,
     "picture_url": "https://images-na.ssl-images-amazon.com/images/I/710H4c%2BmDeL.jpg"
 };
 var movie1 = {
     "name": "Brooklyn Nine-Nine",
-    "category": "movie",
+    "category": "Movie",
     "price": 200,
     "picture_url": "https://m.media-amazon.com/images/M/MV5BMGU3NGYyYTYtYjIzMS00ZmUwLTlmMjAtZDhhMzFiNWI0NzNkXkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_.jpg"
 };
 var movie2 = {
     "name": "La Casa De Papel",
-    "category": "movie",
+    "category": "Movie",
     "price": 150,
     "picture_url": "https://i.ebayimg.com/images/i/163093272041-0-1/s-l1000.jpg"
 };
 var movie3 = {
     "name": "The Good Place",
-    "category": "movie",
+    "category": "Movie",
     "price": 200,
     "picture_url": "https://images-na.ssl-images-amazon.com/images/I/91DHcyLCfaL._SL1500_.jpg"
 };
 var album1 = {
     "name": "Don't Smile At Me",
-    "category": "album",
+    "category": "Album",
     "price": 300,
     "picture_url": "https://cdn.shopify.com/s/files/1/2171/7009/products/dontsmile-vinyl_grande.png?v=1532122195"
 };
 var album2 = {
     "name": "Solstice",
-    "category": "album",
+    "category": "Album",
     "price": 250,
     "picture_url": "https://sidekick-music.com/wp-content/uploads/2018/09/Down.jpg"
 };
 var album3 = {
     "name": "Tame Impala",
-    "category": "album",
+    "category": "Album",
     "price": 100,
     "picture_url": "https://dx72k0ec4onep.cloudfront.net/product/1616/28332732/8WQFMW-1478305742-1280x1280-602547394897.jpg"
 };
@@ -97,6 +97,26 @@ $('#album-3 .name').text(album3.name);
 $('#album-3 .category').text(album3.category);
 $('#album-3 .price').text(album3.price);
 $('#album-3 img').attr('src', album3.picture_url);
+
+//code to create the bootstrap grid and place it in the HTML doc,
+//it works, just need to add all the objects to it:
+
+for(var i=0; i < 3; i++) {
+    var row = $("<div class='row pb-5'></div>");
+    for(var j=0; j < 3; j++) {
+        row.append("<div class=col-sm></div>");
+        //to do: for each column append one object so all the products are placed in the grid with jquery
+    }
+    $("#content").append(row);
+}
+
+// code for adding one value from an object to HTML, not important right now
+// function add_to_page() {
+//     $('#content').append($('<div id=book-1>').html($('<div class=name>').text(book1.name)))
+// add_to_page(book1);
+
+
+//code for search:
 
 $("#userInput").on("keyup", function () {
     var searchInput = $(this).val().toLowerCase();
